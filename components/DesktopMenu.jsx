48c59da8f1fe1d59119b7a85ce181007d9ca491f
Md Atikuzzaman <akash203037@gmail.com>
2024-11-13 13:38:32 +0600

    fil

--- components/DesktopMenu.jsx
+++ components/DesktopMenu.jsx
@@ -1,63 +1,26 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 import React from "react";
 
 const DesktopMenu = () => {
-  const path = usePathname();
+//   const path = usePathname();
   return (
     <ul className="flex items-center gap-4">
       <li>
-        <Link
-          href={"/"}
-          className={path == "/" ? "text-[#5A36FF] font-semibold" : ""}
-        >
-          Home
-        </Link>
+        <Link href={"/"}>Home</Link>
       </li>
       <li>
-        <Link
-          href={"/about"}
-          className={path == "/about" ? "text-[#5A36FF] font-semibold" : ""}
-        >
-          About
-        </Link>
+        <Link href={"/about"}>About</Link>
       </li>
       <li>
-        <Link
-          href={"/service"}
-          className={path == "/service" ? "text-[#5A36FF] font-semibold" : ""}
-        >
-          Service
-        </Link>
+        <Link href={"/service"}>Service</Link>
       </li>
       <li>
-        <Link
-          href={"/testimonials"}
-          className={
-            path == "/testimonials" ? "text-[#5A36FF] font-semibold" : ""
-          }
-        >
-          Testimonials
-        </Link>
+        <Link href={"/testimonials"}>Testimonials</Link>
       </li>
       <li>
-        <Link
-          href={"/case-study"}
-          className={
-            path == "/case-study" ? "text-[#5A36FF] font-semibold" : ""
-          }
-        >
-          Case study
-        </Link>
-      </li>
-      <li>
-        <Link
-          href={"/blog"}
-          className={path == "blog" ? "text-[#5A36FF] font-semibold" : ""}
-        >
-          Blog
-        </Link>
+        <Link href={"/case-study"}>Case study</Link>
       </li>
     </ul>
   );
