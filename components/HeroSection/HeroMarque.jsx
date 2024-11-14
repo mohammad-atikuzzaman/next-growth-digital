88a90f9011a185a47ff7569e5edae7ad916d32f0
Md Atikuzzaman <akash203037@gmail.com>
2024-11-14 13:20:45 +0600

    marque animation addd

--- components/HeroSection/HeroMarque.jsx
+++ components/HeroSection/HeroMarque.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import Marquee from "react-fast-marquee";
 
+import { FaBitcoin } from "react-icons/fa";
+
 const HeroMarque = () => {
     return (
-        <Marquee>
+        <Marquee className='container mx-auto mt-16' pauseOnHover={true}>
+            <div className='ml-6 flex items-center gap-4 bg-purple-200 px-2 rounded-md text-gray-200'>
+                <FaBitcoin className='text-2xl'/>
+                <h2 className='text-3xl font-bold'>Coinbase</h2>
+            </div>
+            <div className='ml-6 flex items-center gap-4 bg-purple-200 px-2 rounded-md text-gray-200'>
+                <FaBitcoin className='text-2xl'/>
+                <h2 className='text-3xl font-bold'>Coinbase</h2>
+            </div>
+            <div className='ml-6 flex items-center gap-4 bg-purple-200 px-2 rounded-md text-gray-200'>
+                <FaBitcoin className='text-2xl'/>
+                <h2 className='text-3xl font-bold'>Coinbase</h2>
+            </div>
+            <div className='ml-6 flex items-center gap-4 bg-purple-200 px-2 rounded-md text-gray-200'>
+                <FaBitcoin className='text-2xl'/>
+                <h2 className='text-3xl font-bold'>Coinbase</h2>
+            </div>
+            <div className='ml-6 flex items-center gap-4 bg-purple-200 px-2 rounded-md text-gray-200'>
+                <FaBitcoin className='text-2xl'/>
+                <h2 className='text-3xl font-bold'>Coinbase</h2>
+            </div>
+            <div className='ml-6 flex items-center gap-4 bg-purple-200 px-2 rounded-md text-gray-200'>
+                <FaBitcoin className='text-2xl'/>
+                <h2 className='text-3xl font-bold'>Coinbase</h2>
+            </div>
             
         </Marquee>
     );
