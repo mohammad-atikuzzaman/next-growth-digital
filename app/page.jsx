f7d7b4b90b64d3c8cb7c58507a6a0d6de4ce69f0
Md Atikuzzaman <akash203037@gmail.com>
2024-11-19 22:38:08 +0600

    swiper added

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import AboutCompany from "@/components/aboutCompany/AboutCompany";
 import AboutUs from "@/components/aboutus/AboutUs";
 import Hero from "@/components/HeroSection/Hero";
 import Services from "@/components/services/Services";
+import Testimonials from "@/components/testimonials/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Services/>
       <AboutUs/>
       <AboutCompany/>
+      <Testimonials/>
     </main>
   );
 }
